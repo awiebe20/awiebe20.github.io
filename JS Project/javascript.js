38309984283543c89fde79fce389b8921f18c084
Austin <awiebe20@georgefox.edu>
2023-11-08 21:07:52 -0800

    added annoying solution arrow.

--- JS Project/javascript.js
+++ JS Project/javascript.js
@@ -1,95 +1,136 @@
-dragElement(document.getElementById("mydiv"));
+dragElement(document.getElementById("menu"));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
+  if (document.getElementById(elmnt.id + "header")) 
+  {
     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
+  } 
+  else 
+  {
     elmnt.onmousedown = dragMouseDown;
   }
 
   function dragMouseDown(e) {
     e = e || window.event;
     e.preventDefault();
-    // get the mouse cursor position at startup:
     pos3 = e.clientX;
     pos4 = e.clientY;
     document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
   }
 
   function elementDrag(e) {
     e = e || window.event;
     e.preventDefault();
-    // calculate the new cursor position:
     pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
-    // set the element's new position:
     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
   }
 
   function closeDragElement() {
-    // stop moving when mouse button is released:
     document.onmouseup = null;
     document.onmousemove = null;
   }
 }
 
-let minute = 0; 
-let second = 0;
+let minutes = 0; 
+let seconds = 0;
+let count = 0;
 
-function stopWatch() {
+function timer() {
     count++;
 
     if (count == 1)
     {
-        second++;
+        seconds++;
         count = 0;
     }
 
-    if (second == 60)
+    if (seconds == 60)
     {
-        minute++;
-        second = 0;
+        minutes++;
+        seconds = 0;
     }
 
-    let minString = minute;
-    let secString = second;
+    let minuntesOutput = minutes;
+    let secondsOutput = seconds;
 
-    if (minute < 10) 
+    if (minutes < 10) 
     {
-        minString = "0" + minString;
+        minuntesOutput = "0" + minuntesOutput;
     }
 
-    if (second < 10)
+    if (seconds < 10)
     {
-        secString = "0" + secString;
+        secondsOutput = "0" + secondsOutput;
     }
 
-    document.getElementById('min').innerHTML = minString;
-    document.getElementById('sec').innerHTML = secString;
+    document.getElementById('min').innerHTML = minuntesOutput;
+    document.getElementById('sec').innerHTML = secondsOutput;
 }
 
-setInterval(stopWatch, 1000);
+setInterval(timer, 1000);
 
-let i = 0;
+let hasArrow = false;
+let i = -1;
 function newWaldo()
 {
+    i++;
     if (i <= 2)
     {
         let imagesrc = ["https://wallpapercave.com/wp/wp7156924.jpg", "https://i.redd.it/4xb7zuo98hp61.jpg", "https://wallpapers.com/images/hd/where-s-waldo-land-of-fruits-zav6dvoyp290celi.jpg"];
         let imagecoords = ["454,1528,491,1591", "686,506,768,612", "1694,805,1728,861"]
         document.getElementById("gameimage").src = imagesrc[i];
         document.getElementById("Waldo").coords = imagecoords[i];
-        i++;
     }
     else {
         location.href = "endpage.html"
     }
+    if (i == 2)
+    {
+        document.getElementById("GameBody").style.zoom = "70%";
+    }
+    document.getElementById("solutionArrow").remove();
+    hasArrow = false;
+}
+
+function solution()
+{
+    if (!hasArrow)
+    {
+        let solutionArrow = document.createElement('img');
+        solutionArrow.src = "https://pngimg.com/d/red_arrow_PNG73.png";
+        solutionArrow.setAttribute('id', 'solutionArrow');
+        document.getElementById("showWaldo").appendChild(solutionArrow);
+        let solutionArrow2 = document.getElementById("solutionArrow");
+        solutionArrow2.style.position = "absolute";
+        solutionArrow2.style.width = "150px";
+        if (i == -1)
+        {
+        solutionArrow2.style.right = "33%";
+        solutionArrow2.style.top = "40%";
+        }
+    if (i == 0)
+        {
+        solutionArrow2.style.left = "19.5%";
+        solutionArrow2.style.top = "98%";
+        }
+    if (i == 1)
+        {
+        solutionArrow2.style.left = "32%";
+        solutionArrow2.style.top = "28%";
+        }
+    if (i == 2)
+        {
+        solutionArrow2.style.right = "15.5%";
+        solutionArrow2.style.bottom = "20%";
+        solutionArrow2.style.transform = "rotate(270deg)";
+        }
+    }
+    
+    hasArrow = true;
 }
